da034c61c6704133f7d9af4e1f32451a31a77ead
limonadi-lemon <phamchauanh1204@gmail.com>
2024-08-25 11:57:04 +0700

    a

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var imageIndex: Int = 1
+    func updateImage() {
+        goodNightImage.image = UIImage(named: "goodNight-Images/goodnight\(imageIndex)")
+       
+        
+    }
+   
 
+    @IBAction func btnNext(_ sender: Any) {
+        imageIndex = imageIndex + 1
+        updateImage()
+        if imageIndex <= 36 {
+            imageIndex = imageIndex + 1
+            updateImage()
+            
+        }
+        else {
+            imageIndex = 1
+            updateImage()
+        }
+    }
+    @IBOutlet weak var goodNightImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      updateImage()
     }
-
-
+    
 }
 
